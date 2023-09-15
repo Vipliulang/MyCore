c2d0b100a17a32f742a9af9795245694b85972a8
刘浪 <1282845210@qq.com>
2023-09-15 13:45:12 +0800

    测试

---
+++ mycore/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+
+namespace mycore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStatus()
+        {
+            return "版本1.0";
+        }
+    }
+}
